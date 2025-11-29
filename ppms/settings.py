c47c668a8bacb46a6287c89390df54157846a79f
Geoffrey Mimano <soyfactor@gmail.com>
2025-11-29 12:40:12 +0300

    Adding db settings, requirements file and setting up .gitignore

--- ppms/settings.py
+++ ppms/settings.py
@@ -72,13 +72,22 @@ WSGI_APPLICATION = "ppms.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ppms',
+        'USER': 'ppms',
+        'PASSWORD': 'k3ester4',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
